4b8c9b7b172b46040f4fdc0b2731c6198eac2b66
Lewis <lewis.sdick@gmail.com>
2015-03-24 23:20:33 +0000

    Upload blog to new account

---
+++ index.php
@@ -0,0 +1,40 @@
+<!DOCTYPE html>
+<html>
+<head>
+<title>Home page</title>
+<link rel="stylesheet" type=text/css href="stylesheet.css">
+</head>
+<body>
+
+<div id="container">
+
+<a href="ips&subnets.html"><img src="ips&subnets.png" alt="IP Addresses and Subnets"></a>
+<p>The IP Addresses and Subnets guide delves into the 
+basics behind Internet Protocol addresses. It covers their 
+purposes, how they are allocated to networks in terms of 
+address quantity, Classless Inter-Domain Routing (CIDR),
+subnets, subnet quantity in a network and host quantity per 
+subnet.
+</p>
+
+<a href="cplusplus.html"><img src="c++.png" alt="C++"></a>
+<p>This page covers all the basics of programming in the highly 
+useful programming language 'C++'.</p>
+
+<a href="windowsconfig.html"><img src="windows7configuration.png" alt="Windows 7 Configuration"></a>
+
+<?php
+	$name = "Jack";
+	
+	if ($name == "Jack") {
+		echo "Hello, Jack";
+	}
+	
+	else {
+		echo  "Who are you?";
+	}
+?>
+	
+</div>
+</body>
+</html>
